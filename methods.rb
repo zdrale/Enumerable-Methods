62d5fbf83c91a26026d79f0343abf89eeca2cfd7
zdrale <nikolazd22@outlook.com>
2020-11-05 14:21:27 +0100

    Added my_count method

--- methods.rb
+++ methods.rb
@@ -22,69 +22,77 @@ module Enumerable
     arry
   end
 
-end
-
 # ***************************     my_all     *********************************
 
 
-def my_all?(arg = nil)
-  output = false
+  def my_all?(arg = nil)
+    output = false
 
-  filtered_array = if !arg # no arguments
+    filtered_array = if !arg # no arguments
 
-  block_given? ? my_select { |el| yield(el) } : my_select { |el| el }
-  elsif arg.is_a?(Regexp)
-    my_select { |el| arg.match(el) }
-  elsif arg.is_a?(Class)
-    my_select { |el| el.class <= arg }
-  else
-    my_select { |el| el == arg }
+    block_given? ? my_select { |el| yield(el) } : my_select { |el| el }
+    elsif arg.is_a?(Regexp)
+      my_select { |el| arg.match(el) }
+    elsif arg.is_a?(Class)
+      my_select { |el| el.class <= arg }
+    else
+      my_select { |el| el == arg }
+    end
+    output = true if filtered_array == to_a
+    output
   end
-  output = true if filtered_array == to_a
-  output
-end
 
 
 # ***************************     my_any     *********************************
 
 
 
-def my_any?(arg = nil)
-  output = false
+  def my_any?(arg = nil)
+    output = false
 
-  filtered_array = if !arg # no arguments
+    filtered_array = if !arg # no arguments
 
-  block_given? ? my_select { |el| yield(el) } : my_select { |el| el }
-  elsif arg.is_a?(Regexp)
-    my_select { |el| arg.match(el) }
-  elsif arg.is_a?(Class)
-    my_select { |el| el.class <= arg }
-  else
-    my_select { |el| el == arg }
+    block_given? ? my_select { |el| yield(el) } : my_select { |el| el }
+    elsif arg.is_a?(Regexp)
+      my_select { |el| arg.match(el) }
+    elsif arg.is_a?(Class)
+      my_select { |el| el.class <= arg }
+    else
+      my_select { |el| el == arg }
+    end
+    output = true unless filtered_array.to_a.empty?
+    output
   end
-  output = true unless filtered_array.to_a.empty?
-  output
-end
 
 
 # ***************************     my_none     ********************************
 
 
 
-def my_none?(arg = nil)
-  output = false
+  def my_none?(arg = nil)
+    output = false
 
-  filtered_array = if !arg
+    filtered_array = if !arg
 
-  block_given? ? my_select { |el| yield(el) } : my_select { |el| el }
-  elsif arg.is_a?(Regexp)
-    my_select { |el| arg.match(el) }
-  elsif arg.is_a?(Class)
-    my_select { |el| el.class <= arg }
-  else
-    my_select { |el| el == arg }
+    block_given? ? my_select { |el| yield(el) } : my_select { |el| el }
+    elsif arg.is_a?(Regexp)
+      my_select { |el| arg.match(el) }
+    elsif arg.is_a?(Class)
+      my_select { |el| el.class <= arg }
+    else
+      my_select { |el| el == arg }
+    end
+    output = true if filtered_array.to_a.empty?
+    output
   end
-  output = true if filtered_array.to_a.empty?
-  output
-end
 
+  def my_count 
+    count = 0
+    if self.size > 1
+      count += 1
+    end
+    yield count
+  end
+  
+
+end
